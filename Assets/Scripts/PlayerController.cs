dbc4ef47271d11c0fffd4ffd93cb8ab39eaa9145
Sophie Suh <sungyeon.suh@gmail.com>
2015-09-06 23:57:48 -0700

    Added triggers to a player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,32 +2,40 @@
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
-
+	
 	// TODO: Make them dynamic
-	public float playerCentreLBound = -1.5f;
-	public float playerCentreRBound = 1.5f;
+	public float playerCentreLBound = -1.5f;	// left bound of the centre of the body
+	public float playerCentreRBound = 1.5f;		// right bound of the centre of the body
 
-	void OnMouseUp(){
+	private Animator animator;					// a reference to the player's animator component
 
-		Vector3 mouseWorldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-		Vector2 playerToMouse = mouseWorldPoint - transform.position;
 
-		if (playerToMouse.x > playerCentreRBound) {
-			Debug.Log("Right!");
-		} else if (playerToMouse.x < playerCentreLBound) {
-			Debug.Log("Left!");
-		} else {
-			Debug.Log("Centre!");
-		}
-
-	}
 	// Use this for initialization
 	void Start () {
+
+		animator = GetComponent<Animator> ();
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	void OnMouseUp(){
+		
+		Vector3 mouseWorldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		Vector2 playerToMouse = mouseWorldPoint - transform.position;
+		
+		if (playerToMouse.x > playerCentreRBound) {
+			animator.SetTrigger("playerRSlap");
+			Debug.Log("Right!");
+		} else if (playerToMouse.x < playerCentreLBound) {
+			animator.SetTrigger("playerLSlap");
+			Debug.Log("Left!");
+		} else {
+			Debug.Log("Centre!");
+		}
+		
 	}
 }
