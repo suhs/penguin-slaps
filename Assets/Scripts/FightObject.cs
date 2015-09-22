a7e6164f1a1194ba82e0b1becc6bfde9ec6936b7
Sophie Suh <sungyeon.suh@gmail.com>
2015-09-21 23:18:47 -0700

    Added variables
    Added variables for character stats, attack stats for both right and left arms

--- Assets/Scripts/FightObject.cs
+++ Assets/Scripts/FightObject.cs
@@ -3,21 +3,44 @@ using System.Collections;
 
 public abstract class FightObject : MonoBehaviour {
 
-	// Default stats
+
+	// stats of a character
 	public float attackSpeed = 0.8f;		// Delay time between attack moves 
 	public float damage = 10.0f;			// Damage each attack can do to opponents
 	public float defense = 1.0f;			// Points that can be subtracted from the total damage points
 	public float criticalRate = 0.05f;		// Critical hit rate
-	public float spirit = 0.01f;			// probability of surviving after a deadly attack with 1 life point remained
+	public float spirit = 0.01f;			// Probability of surviving after a deadly attack with 1 life point remained
 
-	public float health = 100.0f;			// total health points
-	public float rage = 0.0f;				// initial rage points
+	// attack stats
+	private bool canAttack = false;			// Indicate whether the object is ready to attack
+	public float cooldownTimeR = 1.0f;		// Cool-down time between attacks for a right arm
+	public float cooldownTimeL = 1.0f;		// Cool-down time between attacks for a left arm
+	public float armPowerR = 1.0f;			// Right arm power (1 at full strength)
+	public float armPowerL = 1.0f;			// Left arm power (1 at full strength)
+	public int stiffnessR = 3;				// Number of attacks required to trigger stiffness on a right arm 
+	public int stiffnessL = 3;				// Number of attacks required to trigger stiffness on a left arm
 
-	private SpriteRenderer spriteRenderer;	//Store a component reference to the attached SpriteRenderer.
+	public char armDominance;				// Arm dominance ('R' or 'L')			
+
+	// status during the fight
+	public float health = 100.0f;			// Total health points
+	public float rage = 0.0f;				// Initial rage points
+
+	private SpriteRenderer spriteRenderer;	// Store a component reference to the attached SpriteRenderer.
+
+	// opponent components
+	public GameObject opponent;				// Reference to an opponent
+	private string opponentTag;				// Tag name of an opponent
 
 
 	// can be overridden by inheriting classes
 	protected virtual void Start () {
+		// inheriting class should assign opponentTag here
+		// find an opponent object
+		opponent = GameObject.FindWithTag (opponentTag);
+		// get a reference to the SpriteRenderer component
+		spriteRenderer = gameObject.GetComponent<SpriteRenderer>(); 
+
 
 	}
 
@@ -28,6 +51,7 @@ public abstract class FightObject : MonoBehaviour {
 	}
 
 	protected virtual void dodge () {
+
 
 	
 	}
@@ -41,9 +65,12 @@ public abstract class FightObject : MonoBehaviour {
      */
 	
 	protected IEnumerator flashObject (SpriteRenderer sprite, int times, float delay) {
-		spriteRenderer.enabled = false;
-		yield return new WaitForSeconds(delay);
-		spriteRenderer.enabled = true;
+		for (int i = 0; i < times; i++) {
+			yield return new WaitForSeconds (delay);
+			spriteRenderer.enabled = false;
+			yield return new WaitForSeconds (delay);
+			spriteRenderer.enabled = true;
+		}
 	}
 
 	// Update is called once per frame
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,7 @@ public class PlayerController : MonoBehaviour {
 	// TODO: Make them dynamic
 	public float playerCentreLBound;	// left bound of the centre of the body
 	public float playerCentreRBound;	// right bound of the centre of the body
-	public float playerDodgeAmount;
+	public float playerDodgeAmount;		// how much a player move when dodge 
 
 	private Animator animator;			// a reference to the player's animator component
 
@@ -24,11 +24,12 @@ public class PlayerController : MonoBehaviour {
 	}
 
 	void OnMouseDown() {
-
+		// get the position with respect to the pivot
 		Vector3 mouseWorldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-		Vector2 playerToMouse = mouseWorldPoint - transform.position;				// get the position with respect to the pivot
+		Vector2 playerToMouse = mouseWorldPoint - transform.position;				
 
 		if (playerToMouse.x >= playerCentreLBound && playerToMouse.x <= playerCentreRBound) {
+			// clicked body; dodge attack while dragging
 			animator.SetTrigger("playerDodge");
 			transform.position += Vector3.down * playerDodgeAmount;
 			Debug.Log ("Centre!");
@@ -38,13 +39,18 @@ public class PlayerController : MonoBehaviour {
 	void OnMouseUp(){
 		
 		Vector3 mouseWorldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-		Vector2 playerToMouse = mouseWorldPoint - transform.position;				// get the position with respect to the pivot
-		
+		// get the position with respect to the pivot
+		Vector2 playerToMouse = mouseWorldPoint - transform.position;
+
+
 		if (playerToMouse.x > playerCentreRBound) {
+			// clicked right wing; slap with a right wing
 			animator.SetTrigger("playerRSlap");
 		} else if (playerToMouse.x < playerCentreLBound) {
+			// clicked left wing; slap with a left wing
 			animator.SetTrigger("playerLSlap");
 		} else {
+			// clicked body; go back and idle
 			animator.SetTrigger("playerIdle");
 			transform.position += Vector3.up * playerDodgeAmount;
 		}
