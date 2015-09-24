bbb6133c63875d02ecbfbd8f7541befdf9aeb947
Sophie Suh <sungyeon.suh@gmail.com>
2015-09-24 14:04:25 -0700

    No major changes - backup purpose only

--- Assets/Scripts/BaseStat.cs
+++ Assets/Scripts/BaseStat.cs
@@ -5,32 +5,50 @@ public class BaseStat : MonoBehaviour {
 	
 	private int _health;			// Total health points
 
-	private float _attackSpeed;		// Delay time between attack moves 
+	private float _speed;		// Delay time between attack moves 
 	private int _strength;			// Damage each attack can do to opponents
 	private int _defense;			// Points that can be subtracted from the total damage points
 	private float _criticalRate;	// Critical hit rate
 	private float _spirit;			// Probability of surviving after a deadly attack with 1 life point remained
+
+	private bool _canAttack;		// Indicate whether the object is ready to attack
 	private float _armPower;		// Arm power (1 if a right arm has full power; -1 if a left arm has full power)
+	private float _cooldownTimeR;	// Cool-down time between attacks for a right arm
+	private float _cooldownTimeL;	// Cool-down time between attacks for a left arm
+	private int _stiffnessR;		// Number of attacks required to trigger stiffness on a right arm 
+	private int _stiffnessL;		// Number of attacks required to trigger stiffness on a left arm 
+
+	private int _burnoutCount;		// Number of attacks that cause 'burnout'
+	private float _burnoutDuration;	// Duration an object have to wait to 
 
 	public BaseStat () {
 		_health = 100;
+		_canAttack = false;
 		
-		_attackSpeed = 0.8f;
+		_speed = 0.8f;
 		_strength = 10;
 		_defense = 1;
 		_criticalRate = 0.05f;
 		_spirit = 0.01f;
+
 		_armPower = 0.0f;	// balanced
+		_cooldownTimeR = 1.0f;
+		_cooldownTimeL = 1.0f;
+		_stiffnessR = 3;
+		_stiffnessL = 3;
+
+
 	}
+	
 
 	public int Health {
 		get { return _health; }
 		set { _health = value; }
 	}
 
-	public float AttackSpeed {
-		get { return _attackSpeed; }
-		set { _attackSpeed = value; }
+	public float Speed {
+		get { return _speed; }
+		set { _speed = value; }
 	}
 
 	public int Strength {
@@ -53,9 +71,36 @@ public class BaseStat : MonoBehaviour {
 		set { _spirit = value; }
 	}
 
+	// Attack-related auto property
+
+	public bool CanAttack {
+		get { return _canAttack; }
+		set { _canAttack = value; }
+	}
+
 	public float ArmPower {
 		get { return _armPower; }
 		set { _armPower = value; }
 	}
+
+	public float CoolDownTimeR {
+		get { return _cooldownTimeR; }
+		set { }
+	}
 	
+	public float CoolDownTimeL {
+		get { return _cooldownTimeL; }
+		set { }
+	}
+	
+	public int StiffnessR {
+		get { return _stiffnessR; }
+		set { }
+	}
+	
+	public int StiffnessL {
+		get { return _stiffnessL; }
+		set { }
+	}
+
 }
--- Assets/Scripts/PlayerStat.cs
+++ Assets/Scripts/PlayerStat.cs
@@ -3,51 +3,16 @@ using System.Collections;
 
 public class PlayerStat : BaseStat {
 
-	private bool _canAttack;			// Indicate whether the object is ready to attack
-	private float _cooldownTimeR;		// Cool-down time between attacks for a right arm
-	private float _cooldownTimeL;		// Cool-down time between attacks for a left arm
-	private int _stiffnessR;			// Number of attacks required to trigger stiffness on a right arm 
-	private int _stiffnessL;			// Number of attacks required to trigger stiffness on a left arm 
-	
-	private int _rage;				// Current rage points
+	private int _rage;					// Current rage points
 	private int _maxRage;				// Maximum rage points
 
 	public PlayerStat () {
-		_canAttack = false;
-		_cooldownTimeR = 1.0f;
-		_cooldownTimeL = 1.0f;
-		_stiffnessR = 3;
-		_stiffnessL = 3;
 
 		_rage = 0;
 		_maxRage = 100;
-	}
 
-	public bool CanAttack {
-		get { return _canAttack; }
-		set { _canAttack = value; }
 	}
-
-	public float CoolDownTimeR {
-		get { return _cooldownTimeR; }
-		set { }
-	}
-
-	public float CoolDownTimeL {
-		get { return _cooldownTimeL; }
-		set { }
-	}
-
-	public int StiffnessR {
-		get { return _stiffnessR; }
-		set { }
-	}
-
-	public int StiffnessL {
-		get { return _stiffnessL; }
-		set { }
-	}
-
+	
 	public int CurrentRage {
 		get { return _rage; }
 		set { }
@@ -57,4 +22,5 @@ public class PlayerStat : BaseStat {
 		get { return _maxRage; }
 		set { }
 	}
+
 }
